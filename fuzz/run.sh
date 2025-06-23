dbbfe110757ba1d1504e1cc1e6e0a98ecf406cd9
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2025-06-23 20:04:52 +0200

    Fix typo (-M/-S) in run.sh

--- fuzz/run.sh
+++ fuzz/run.sh
@@ -2,9 +2,9 @@
 
 let CORES=$(nproc)-1
 
+AFL_MAP_SIZE=651557 AFL_SKIP_BIN_CHECK=1 afl-fuzz -i in/ -o out -M fuzzer0  -- ./harness.sh @@ > /dev/null &
+
 for i in $(seq 1 $CORES);
 do
-    AFL_MAP_SIZE=651557 AFL_SKIP_BIN_CHECK=1 afl-fuzz -i in/ -o out -M fuzzer$i  -- ./harness.sh @@ > /dev/null &
+    AFL_MAP_SIZE=651557 AFL_SKIP_BIN_CHECK=1 afl-fuzz -i in/ -o out -S fuzzer$i  -- ./harness.sh @@ > /dev/null &
 done
-
-AFL_MAP_SIZE=651557 AFL_SKIP_BIN_CHECK=1 afl-fuzz -i in/ -o out -M fuzzer0  -- ./harness.sh @@ > /dev/null &
